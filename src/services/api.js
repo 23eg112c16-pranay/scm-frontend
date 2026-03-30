21063040f4ed21415a760fd44ec9c8f80f00a1aa
23eg112c16-pranay <23eg112c16@anurag.edu.in>
2026-03-30 11:44:16 +0530

    change backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://scm-backend-ygsj.onrender.com/api";
 
 const request = async (endpoint, options = {}) => {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
